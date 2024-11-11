ce83f6d9dbe3f581c890aadb999f64d6ee817bdb
Husain1771 <husainamirza@gmail.com>
2024-11-11 13:42:04 +0300

    Changed Table Name

--- QuestionScript/dynamodbFiller.py
+++ QuestionScript/dynamodbFiller.py
@@ -6,7 +6,7 @@ client = boto3.client('dynamodb', region_name='us-east-1')
 
 TransactItemss = [] # List of items to be added to the table
 indexes = {} # Dictionary to store the indexes of the questions which contains the list of sort keys of the questions for each type
-TableName = 'mahawaj-codecatalyst-sst-app-Records'   # Channge the table name
+TableName = 'prod-codecatalyst-sst-app-Records'   # Channge the table name
 QuestionTypes = ["writing", "reading", "listening", "speaking"]
 
 questions = json.loads(open("./questionsNew.json", "r", encoding="utf8").read())
