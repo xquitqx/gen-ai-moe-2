236236dae05914727230a4754f65dab7649903fa
Mohamed Khalaf <mjkhalaf13@gmail.com>
2024-12-06 16:51:59 +0300

    Textract is working fine

---
+++ stacks/StorageStack.ts
@@ -0,0 +1,28 @@
+import { Bucket, Function, StackContext } from "sst/constructs";
+
+export function StorageStack({ stack }: StackContext) {
+  // Create the Lambda function
+  const notificationFunction = new Function(stack, "NotificationFunctionPY", {
+    handler: "packages/functions/src/TextNotification.main",
+    timeout: 900,
+    runtime: "python3.9", 
+    permissions: ["textract:AmazonTextractFullAccesss","s3:GetObject" ,"textract:StartDocumentAnalysis", "textract:GetDocumentAnalysis"],
+  });
+
+  // Create the S3 bucket and set up notifications
+  const bucket = new Bucket(stack, "BucketTextract", {
+    notifications: {
+      myNotification: {
+        function: notificationFunction,
+        events: ["object_created"], 
+      },
+    },
+  });
+  // Outputs
+  stack.addOutputs({
+    BucketName: bucket.bucketName,
+    LambdaFunctionName: notificationFunction.functionName,
+  });
+
+  return { bucket , notificationFunction};
+}
