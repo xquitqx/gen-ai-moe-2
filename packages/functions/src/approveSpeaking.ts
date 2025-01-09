bd5b48ad2e906012f22801f8f527d30d4424710d
Mohamed Khalaf <mjkhalaf13@gmail.com>
2025-01-09 17:25:23 +0300

    uploading listening and speajing  data into db

--- packages/functions/src/approveSpeaking.ts
+++ packages/functions/src/approveSpeaking.ts
@@ -22,9 +22,174 @@ export const handler: APIGatewayProxyHandler = async (event) => {
           console.log("events:" , event.body);
           const parsedBody = JSON.parse(JSON.parse(event.body))
           console.log("DOUBLE PARSE:", parsedBody)
+
+          const objectNames = parsedBody[2].map((url: string) => {
+            const parts = url.split('/');
+            return `${parts[parts.length - 2]}/${parts[parts.length - 1]}`; // Combine "Speaking" and file name
+        });
+
+        const firstFiveSentences = parsedBody[0][0]
+         .split('.\n') 
+         .slice(0, 5); 
+        
     
           const transactItems: any[] = [];
-        
+          let id = uuidv4();
+          let checker = true;
+          while(checker) {
+            const check = await dynamodb
+            .query({
+              TableName: tableName,
+              KeyConditionExpression: 'PK = :pk AND SK = :sk',
+              ExpressionAttributeValues: {
+                ':pk': { S: 'speaking' },  // String value for PK
+                ':sk': { S: id },   // String value for SK
+                },
+                ProjectionExpression: 'PK, SK',
+            })
+            .promise(); 
+            const checkQuestion = check.Items?.[0];
+            if (checkQuestion) {
+              // const sortKey = checkQuestion.SK?.S
+              id = uuidv4();
+            }
+            else {
+              checker = false;
+            }
+           }
+           transactItems.push({
+            Put: {
+                TableName: tableName,
+                Item: {
+                    PK: { S: "speaking" },
+                    SK: { S: id },
+                    P1: {
+                        M: {
+                            Questions: {
+                                L: [
+                                    {
+                                        M: {
+                                            S3Key: { S: `${objectNames[1]}` },
+                                            text: { S: `${parsedBody[1][1]}` },
+                                        }
+                                    },
+                                    {
+                                        M: {
+                                            S3Key: { S: `${objectNames[2]}` },
+                                            text: { S: `${parsedBody[1][2]}` },
+                                        }
+                                    },
+                                    {
+                                        M: {
+                                            S3Key: { S: `${objectNames[3]}` },
+                                            text: { S: `${parsedBody[1][3]}` },
+                                        }
+                                    }
+                                ]
+                            },
+                            Task: {
+                                M: {
+                                    S3Key: { S: `${objectNames[0]}` },
+                                    text: { S: `${parsedBody[1][0]}` },
+                                }
+                            }
+                        }
+                    },
+                    P2: {
+                        M: {
+                            Questions: {
+                                L: [
+                                    {
+                                        M: {
+                                            text: { S: `${firstFiveSentences[0]}` },
+                                        }
+                                    },
+                                    {
+                                        M: {
+                                            text: { S: `${firstFiveSentences[1]}` },
+                                        }
+                                    },
+                                    {
+                                        M: {
+                                            text: { S: `${firstFiveSentences[2]}` },
+                                        }
+                                    },
+                                    {
+                                        M: {
+                                            text: { S: `${firstFiveSentences[3]}` },
+                                        }
+                                    },
+                                    {
+                                      M: {
+                                          text: { S: `${firstFiveSentences[4]}` },
+                                      }
+                                  }
+                                ]
+                            },
+                            Task: {
+                                M: {
+                                    S3Key: { S: `${objectNames[0]}` },
+                                    text: { S: `${parsedBody[1][0]}` },
+                                }
+                            }
+                        }
+                    },
+                    P3: {
+                        M: {
+                            Questions: {
+                                L: [
+                                    {
+                                        M: {
+                                            S3Key: { S: `${objectNames[4]}` },
+                                            text: { S: `${parsedBody[1][4]}` },
+                                        }
+                                    },
+                                    {
+                                        M: {
+                                            S3Key: { S: `${objectNames[5]}` },
+                                            text: { S: `${parsedBody[1][5]}` },
+                                        }
+                                    },
+                                    {
+                                        M: {
+                                            S3Key: { S: `${objectNames[6]}` },
+                                            text: { S: `${parsedBody[1][6]}` },
+                                        }
+                                    }
+                                ]
+                            },
+                            Task: {
+                                M: {
+                                    S3Key: { S: `${objectNames[0]}` },
+                                    text: { S: `${parsedBody[1][0]}` },
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        transactItems.push({
+          Update: {
+              TableName: tableName,
+              Key: { // Key is required for Update
+                  PK: { S: "speaking" },
+                  SK: { S: "index" },
+              },
+              UpdateExpression: "SET #index = list_append(if_not_exists(#index, :empty_list), :new_element)",
+              ExpressionAttributeNames: {
+                  "#index": "index"
+              },
+              ExpressionAttributeValues: {
+                  ":new_element": { L: [{ S: id }] },
+                  ":empty_list": { L: [] } // Important for creating the list if it doesn't exist
+              },
+          },
+      });
+      const transactParams = { TransactItems: transactItems };
+      const command = new TransactWriteItemsCommand(transactParams);
+      const response = await dynamodb.transactWriteItems(transactParams).promise();
+      
     // const transactParams = { TransactItems: transactItems };
     // const command = new TransactWriteItemsCommand(transactParams);
     // const response = await dynamodb.transactWriteItems(transactParams).promise();
