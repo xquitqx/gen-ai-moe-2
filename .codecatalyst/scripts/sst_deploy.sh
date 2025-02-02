9c5a5b1dc4f1a06e6c8c21280f8b214fc940bf01
xquitqx <88673514+xquitqx@users.noreply.github.com>
2025-02-02 13:35:24 +0300

    Update sst_deploy.sh

--- .codecatalyst/scripts/sst_deploy.sh
+++ .codecatalyst/scripts/sst_deploy.sh
@@ -4,10 +4,18 @@ echo "Deploying project"
 source ~/.bashrc
 nohup dockerd &
 docker version
-echo "Before npm Install"
+echo "Before npm Install@@@@@@@@@@@"
 npm install
-echo "Done from npm Install"
+echo "Done from npm Install@@@@@@@@@@@@"
+echo "Before react-dropzone install@@@@@@@@@@@@@"
 npm install react-dropzone
+echo "After react-dropzone install@@@@@@@@@@@@"
+echo "Before @heroicons/react install@@@@@@@@@@@@@"
 npm install @heroicons/react
+echo "After @heroicons/react install@@@@@@@@@@@@"
+echo "Before chart.js install@@@@@@@@@@@@@"
 npm install chart.js
+echo "After chart.js install@@@@@@@@@@@@"
+echo "Starting Deploy sst npx@@@@@@@@@@@@"
 npx sst deploy --stage prod
+echo "Done from Deploy sst npx@@@@@@@@@@@"
