316f82d584e6057d53397f153dc8c0dd4e9f9131
xquitqx <88673514+xquitqx@users.noreply.github.com>
2025-02-03 16:21:12 +0300

    Update sst_deploy.sh

--- .codecatalyst/scripts/sst_deploy.sh
+++ .codecatalyst/scripts/sst_deploy.sh
@@ -27,9 +27,9 @@ npm install -g aws-cdk
 
 # ***** Custom Bootstrap Step with Least-Privilege Policy *****
 echo "Bootstrapping CDK with custom execution policy @@@@@@@@@@@@@@@@@"
-npx cdk bootstrap aws://571600842703/us-east-1 --force --cloudformation-execution-policies arn:aws:iam::571600842703:policy/cdk-toolkit-jamal-least-privillege --parameters PublicAccessBlockConfiguration=false
-# If you need to limit which principals can assume the role, you can also add:
-# --trust arn:aws:iam::571600842703:role/CodeCatalystWorkflowDevelopmentRole-iGA_Jamal_Internal
+npx cdk bootstrap aws://571600842703/us-east-1 --force \
+  --cloudformation-execution-policies arn:aws:iam::571600842703:policy/cdk-toolkit-jamal-least-privillege \
+  --no-public-access-block-configuration
 
 echo "Starting Deploy sst npx@@@@@@@@@@@@"
 npx sst deploy --stage prod
