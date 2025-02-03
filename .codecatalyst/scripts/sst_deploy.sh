3572ffdfdfbe79cb4df9183129248a6f8ae7d2da
xquitqx <88673514+xquitqx@users.noreply.github.com>
2025-02-03 13:59:37 +0300

    Update sst_deploy.sh

--- .codecatalyst/scripts/sst_deploy.sh
+++ .codecatalyst/scripts/sst_deploy.sh
@@ -21,6 +21,10 @@ echo "Before chart.js install@@@@@@@@@@@@@"
 npm install chart.js
 echo "After chart.js install@@@@@@@@@@@@"
 
+echo "cdk version and update check @@@@@@@@@@@@@@@@@@@@@"
+npx cdk --version
+npm install -g aws-cdk
+
 # ***** Custom Bootstrap Step with Least-Privilege Policy *****
 echo "Bootstrapping CDK with custom execution policy @@@@@@@@@@@@@@@@@"
 npx cdk bootstrap aws://571600842703/us-east-1 --force --cloudformation-execution-policies arn:aws:iam::571600842703:policy/cdk-toolkit-jamal-least-privillege -c bootstrapBucketPublicAccessBlock=false
