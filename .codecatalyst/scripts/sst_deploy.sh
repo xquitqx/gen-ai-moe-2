603ce846cfe3a6f3e0d119aa92cf80814e08d429
xquitqx <88673514+xquitqx@users.noreply.github.com>
2025-02-03 16:32:12 +0300

    Update sst_deploy.sh

--- .codecatalyst/scripts/sst_deploy.sh
+++ .codecatalyst/scripts/sst_deploy.sh
@@ -27,9 +27,7 @@ npm install -g aws-cdk
 
 # ***** Custom Bootstrap Step with Least-Privilege Policy *****
 echo "Bootstrapping CDK with custom execution policy @@@@@@@@@@@@@@@@@"
-npx cdk bootstrap aws://571600842703/us-east-1 --force \
-  --cloudformation-execution-policies arn:aws:iam::571600842703:policy/cdk-toolkit-jamal-least-privillege \
-  --no-public-access-block-configuration
+npx cdk bootstrap aws://571600842703/us-east-1 --force --cloudformation-execution-policies arn:aws:iam::571600842703:policy/cdk-toolkit-jamal-least-privillege --no-public-access-block-configuration
 
 echo "Starting Deploy sst npx@@@@@@@@@@@@"
 npx sst deploy --stage prod
