c0670207cb9726e94f03701ec7f595594fa8967d
xquitqx <88673514+xquitqx@users.noreply.github.com>
2025-02-05 16:27:17 +0300

    Update sst_deploy.sh

--- .codecatalyst/scripts/sst_deploy.sh
+++ .codecatalyst/scripts/sst_deploy.sh
@@ -28,28 +28,9 @@ npx sst version
 
 echo "new code for deploying new cdk cdktoolkit and sstbootstrap @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 
-set -euo pipefail
-
-# 1) Deploy the AWS CDK toolkit stack if not already deployed
-aws cloudformation deploy \
-  --template-file cdk-bootstrap-template.yml \
-  --stack-name CDKToolkit \
-  --capabilities CodeCatalystWorkflowDevelopmentRole-iGA_Jamal_Internal \
-  --no-fail-on-empty-changeset
-
-# 2) Deploy the SST bootstrap stack if not already deployed
-aws cloudformation deploy \
-  --template-file sst-bootstrap-template.yml \
-  --stack-name SSTBootstrap \
-  --capabilities CodeCatalystWorkflowDevelopmentRole-iGA_Jamal_Internal \
-  --no-fail-on-empty-changeset
-
-# 3) Finally, deploy your SST app
-#npx sst deploy --stage prod
-
 # ***** Custom Bootstrap Step with Least-Privilege Policy *****
 echo "Bootstrapping CDK with custom execution policy @@@@@@@@@@@@@@@@@"
-#npx cdk bootstrap aws://571600842703/us-east-1 --force --cloudformation-execution-policies arn:aws:iam::571600842703:policy/cdk-toolkit-jamal-least-privillege --no-public-access-block-configuration
+npx cdk bootstrap aws://571600842703/us-east-1 --force --cloudformation-execution-policies arn:aws:iam::571600842703:policy/cdk-toolkit-jamal-least-privillege --no-public-access-block-configuration
 # Explicitly run SST's bootstrap command (instead of letting it auto-bootstrap):
 
 echo "Starting Deploy sst npx@@@@@@@@@@@@"
